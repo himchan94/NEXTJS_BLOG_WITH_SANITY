f3890e651b8eb58d97d3942cb1b8d51a5a54b911
himchan94 <himchan9494@gmail.com>
2022-04-07 00:26:34 +0900

    feat: Applied env & image domian in next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  trailingSlash: false,
+  images: {
+    domains: ["cdn.sanity.io"],
+  },
+  env: {
+    SANITY_PROJECT_ID: "klr1kbxl",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
